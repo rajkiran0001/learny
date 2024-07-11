6dcc4d579ac1703effbe9d5a57c8a421846e302d
rajkiran <rajkiran.one@gmail.com>
2024-07-11 06:31:46 +0200

    - add logo, language button, schedule and format

--- js/script.js
+++ js/script.js
@@ -2,7 +2,34 @@ let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .nav');
 let header = document.querySelector('.header');
 
-menu.onclick = () =>{
-   menu.classList.toggle('fa-times');
-   navbar.classList.toggle('active');
-}
+
+
+$('#menu1 li').click(function(){
+   $('#drop4').html($(this).text())
+
+   var result = $.trim($('#drop4').text());
+   if(result == "Chinese"){
+   }
+   if(result == "English") {
+      $('#drop4').html($(this).text() + `<span class="flag-icon flag-icon-us"></span>`)
+   }
+   if(result == "Chinese") {
+      $('#drop4').html($(this).text() + `<span class="flag-icon flag-icon-ch"></span>`)
+   }
+   if(result == "Spanish") {
+      $('#drop4').html($(this).text() + `<span class="flag-icon flag-icon-es"></span>`)
+   }
+   if(result == "Arabic") {
+      $('#drop4').html($(this).text() + `<span class="flag-icon flag-icon-arab"></span>`)
+   }
+   if(result == "Hindi") {
+      $('#drop4').html($(this).text() + `<span class="flag-icon flag-icon-in"></span>`)
+   }
+   if(result == "French") {
+      $('#drop4').html($(this).text() + `<span class="flag-icon flag-icon-fr"></span>`)
+   }
+   if(result == "German") {
+      $('#drop4').html($(this).text() + `<span class="flag-icon flag-icon-de"></span>`)
+   }
+   
+   })
